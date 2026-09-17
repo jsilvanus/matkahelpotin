78ad5c2d1eea3395b9a5c7bd310c36a75d61fb2d
Juha Itäleino <jsilvanus@gmail.com>
2026-09-18 02:27:24 +0300

    Migrate legacy leg distance sources

--- app/src/main/java/fi/italeino/matkahelpotin/data/local/DatabaseMigrations.kt
+++ app/src/main/java/fi/italeino/matkahelpotin/data/local/DatabaseMigrations.kt
@@ -8,5 +8,11 @@ val MIGRATION_1_2 = object : Migration(1, 2) {
         database.execSQL("ALTER TABLE business_trip_leg ADD COLUMN calculatedDistanceMeters INTEGER")
         database.execSQL("ALTER TABLE business_trip_leg ADD COLUMN calculatedDistanceProvider TEXT")
         database.execSQL("ALTER TABLE business_trip_leg ADD COLUMN manualDistanceOverrideMeters INTEGER")
+        database.execSQL(
+            "UPDATE business_trip_leg SET distanceSource = 'ROUTING_PROVIDER', calculatedDistanceMeters = distanceMeters, calculatedDistanceProvider = 'legacy-map-provider' WHERE distanceSource = 'MAP_PROVIDER'"
+        )
+        database.execSQL(
+            "UPDATE business_trip_leg SET distanceSource = 'MANUAL_OVERRIDE', manualDistanceOverrideMeters = distanceMeters WHERE distanceSource = 'MANUAL'"
+        )
     }
 }
