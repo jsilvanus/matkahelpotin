e089013299d843462a629d13292b3bcbda89b538
Juha Itäleino <jsilvanus@gmail.com>
2026-09-17 20:27:10 +0300

    feat: wire commute repositories

--- app/src/main/java/fi/italeino/matkahelpotin/MatkahelpotinApplication.kt
+++ app/src/main/java/fi/italeino/matkahelpotin/MatkahelpotinApplication.kt
@@ -2,16 +2,14 @@ package fi.italeino.matkahelpotin
 
 import android.app.Application
 import androidx.room.Room
-import fi.italeino.matkahelpotin.data.RoomEmploymentRepository
-import fi.italeino.matkahelpotin.data.RoomPlaceRepository
-import fi.italeino.matkahelpotin.data.RoomVehicleRepository
+import fi.italeino.matkahelpotin.data.*
 import fi.italeino.matkahelpotin.data.local.MatkahelpotinDatabase
 
 class MatkahelpotinApplication : Application() {
-    val database: MatkahelpotinDatabase by lazy {
-        Room.databaseBuilder(this, MatkahelpotinDatabase::class.java, "matkahelpotin.db").build()
-    }
+    val database: MatkahelpotinDatabase by lazy { Room.databaseBuilder(this, MatkahelpotinDatabase::class.java, "matkahelpotin.db").build() }
     val employmentRepository by lazy { RoomEmploymentRepository(database.employmentDao()) }
     val placeRepository by lazy { RoomPlaceRepository(database.placeDao()) }
     val vehicleRepository by lazy { RoomVehicleRepository(database.vehicleDao()) }
+    val commuteProfileRepository by lazy { RoomCommuteProfileRepository(database.commuteProfileDao()) }
+    val commuteRecordRepository by lazy { RoomCommuteRecordRepository(database.commuteRecordDao()) }
 }
