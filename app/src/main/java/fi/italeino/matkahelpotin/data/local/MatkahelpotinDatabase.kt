de97825f40c5182b102885a86eb4f80134db722a
Juha Itäleino <jsilvanus@gmail.com>
2026-09-17 20:26:57 +0300

    feat: expose commute record DAO

--- app/src/main/java/fi/italeino/matkahelpotin/data/local/MatkahelpotinDatabase.kt
+++ app/src/main/java/fi/italeino/matkahelpotin/data/local/MatkahelpotinDatabase.kt
@@ -5,12 +5,10 @@ import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 
 @Database(
-    entities = [
-        EmploymentEntity::class, PlaceEntity::class, EmploymentWorkplaceEntity::class,
+    entities = [EmploymentEntity::class, PlaceEntity::class, EmploymentWorkplaceEntity::class,
         CommuteProfileEntity::class, CommuteRecordEntity::class, BusinessLocationEntity::class,
         RouteEntity::class, BusinessTripEntity::class, BusinessTripLegEntity::class,
-        VehicleEntity::class, ReimbursementRateEntity::class, MileagePolicyEntity::class,
-    ],
+        VehicleEntity::class, ReimbursementRateEntity::class, MileagePolicyEntity::class],
     version = 1,
     exportSchema = true,
 )
@@ -21,5 +19,6 @@ abstract class MatkahelpotinDatabase : RoomDatabase() {
     abstract fun vehicleDao(): VehicleDao
     abstract fun employmentWorkplaceDao(): EmploymentWorkplaceDao
     abstract fun commuteProfileDao(): CommuteProfileDao
+    abstract fun commuteRecordDao(): CommuteRecordDao
     abstract fun businessLocationDao(): BusinessLocationDao
 }
