a27836ac51642c2c04586cea541a43777455311a
Juha Itäleino <jsilvanus@gmail.com>
2026-09-17 20:27:14 +0300

    feat: make commute calendar the main screen

--- app/src/main/java/fi/italeino/matkahelpotin/MainActivity.kt
+++ app/src/main/java/fi/italeino/matkahelpotin/MainActivity.kt
@@ -4,20 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.*
 import androidx.lifecycle.viewmodel.compose.viewModel
-import fi.italeino.matkahelpotin.ui.SettingsScreen
-import fi.italeino.matkahelpotin.ui.SettingsViewModel
+import fi.italeino.matkahelpotin.ui.CommuteScreen
+import fi.italeino.matkahelpotin.ui.CommuteViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        val application = application as MatkahelpotinApplication
+        val app = application as MatkahelpotinApplication
         setContent {
             MaterialTheme {
-                Surface { SettingsScreen(viewModel(factory = SettingsViewModel.factory(application))) }
+                Surface {
+                    CommuteScreen(viewModel(factory = object : androidx.lifecycle.ViewModelProvider.Factory {
+                        @Suppress("UNCHECKED_CAST")
+                        override fun <T : androidx.lifecycle.ViewModel> create(modelClass: Class<T>): T =
+                            CommuteViewModel(app.commuteProfileRepository, app.commuteRecordRepository) as T
+                    }))
+                }
             }
         }
     }
