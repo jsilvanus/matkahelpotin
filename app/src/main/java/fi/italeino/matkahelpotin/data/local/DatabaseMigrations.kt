1f1c42df271a1a30a8497d2689522d9934e8e56b
Juha Itäleino <jsilvanus@gmail.com>
2026-09-18 02:26:55 +0300

    Add Room migration for distance provenance

---
+++ app/src/main/java/fi/italeino/matkahelpotin/data/local/DatabaseMigrations.kt
@@ -0,0 +1,12 @@
+package fi.italeino.matkahelpotin.data.local
+
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
+
+val MIGRATION_1_2 = object : Migration(1, 2) {
+    override fun migrate(database: SupportSQLiteDatabase) {
+        database.execSQL("ALTER TABLE business_trip_leg ADD COLUMN calculatedDistanceMeters INTEGER")
+        database.execSQL("ALTER TABLE business_trip_leg ADD COLUMN calculatedDistanceProvider TEXT")
+        database.execSQL("ALTER TABLE business_trip_leg ADD COLUMN manualDistanceOverrideMeters INTEGER")
+    }
+}
