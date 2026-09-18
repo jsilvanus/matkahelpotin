d06be329cf1ddfd31caad7b29ad7c7e5d9fa6191
Juha Itäleino <jsilvanus@gmail.com>
2026-09-18 07:16:37 +0300

    Register mileage policy database migration

--- app/src/main/java/fi/italeino/matkahelpotin/MatkahelpotinApplication.kt
+++ app/src/main/java/fi/italeino/matkahelpotin/MatkahelpotinApplication.kt
@@ -5,10 +5,12 @@ import androidx.room.Room
 import fi.italeino.matkahelpotin.data.*
 import fi.italeino.matkahelpotin.data.local.MatkahelpotinDatabase
 import fi.italeino.matkahelpotin.data.local.MIGRATION_1_2
+import fi.italeino.matkahelpotin.data.local.MIGRATION_2_3
+import fi.italeino.matkahelpotin.data.local.MIGRATION_3_4
 
 class MatkahelpotinApplication : Application() {
     val database: MatkahelpotinDatabase by lazy { Room.databaseBuilder(this, MatkahelpotinDatabase::class.java, "matkahelpotin.db")
-        .addMigrations(MIGRATION_1_2, MIGRATION_2_3)
+        .addMigrations(MIGRATION_1_2, MIGRATION_2_3, MIGRATION_3_4)
         .build() }
     val employmentRepository by lazy { RoomEmploymentRepository(database.employmentDao()) }
     val placeRepository by lazy { RoomPlaceRepository(database.placeDao()) }
