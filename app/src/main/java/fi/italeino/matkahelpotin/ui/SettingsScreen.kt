4f2974f5bbc658bc62b2587a3b6ea869f32fbfba
Juha Itäleino <jsilvanus@gmail.com>
2026-09-18 08:26:10 +0300

    Import mileage policy scope in settings

--- app/src/main/java/fi/italeino/matkahelpotin/ui/SettingsScreen.kt
+++ app/src/main/java/fi/italeino/matkahelpotin/ui/SettingsScreen.kt
@@ -7,7 +7,7 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import fi.italeino.matkahelpotin.domain.PlaceType
+import fi.italeino.matkahelpotin.domain.*
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
